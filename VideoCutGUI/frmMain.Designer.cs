19c54ac58d42cbd19c4c738edeaf891b541e9e83
qratosone <qratosone@live.com>
2019-03-11 16:30:30 +0800

    update Title

--- VideoCutGUI/frmMain.Designer.cs
+++ VideoCutGUI/frmMain.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("此处显示已完成文件");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("此处显示已完成文件");
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.lblNum = new System.Windows.Forms.Label();
             this.btnDir = new System.Windows.Forms.Button();
@@ -64,6 +64,7 @@
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -78,24 +79,27 @@
             this.splitContainer1.Panel2.Controls.Add(this.gbLog);
             this.splitContainer1.Panel2.Controls.Add(this.gbSave);
             this.splitContainer1.Panel2.Controls.Add(this.gbFreq);
-            this.splitContainer1.Size = new System.Drawing.Size(819, 552);
-            this.splitContainer1.SplitterDistance = 146;
+            this.splitContainer1.Size = new System.Drawing.Size(1116, 803);
+            this.splitContainer1.SplitterDistance = 212;
+            this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 0;
             // 
             // lblNum
             // 
             this.lblNum.AutoSize = true;
-            this.lblNum.Location = new System.Drawing.Point(345, 134);
+            this.lblNum.Location = new System.Drawing.Point(460, 168);
+            this.lblNum.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNum.Name = "lblNum";
-            this.lblNum.Size = new System.Drawing.Size(107, 12);
+            this.lblNum.Size = new System.Drawing.Size(135, 15);
             this.lblNum.TabIndex = 2;
             this.lblNum.Text = "当前已选定0个文件";
             // 
             // btnDir
             // 
-            this.btnDir.Location = new System.Drawing.Point(508, 32);
+            this.btnDir.Location = new System.Drawing.Point(677, 40);
+            this.btnDir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnDir.Name = "btnDir";
-            this.btnDir.Size = new System.Drawing.Size(140, 91);
+            this.btnDir.Size = new System.Drawing.Size(187, 114);
             this.btnDir.TabIndex = 1;
             this.btnDir.Text = "选择目录";
             this.btnDir.UseVisualStyleBackColor = true;
@@ -103,9 +107,10 @@
             // 
             // btnFile
             // 
-            this.btnFile.Location = new System.Drawing.Point(152, 32);
+            this.btnFile.Location = new System.Drawing.Point(203, 40);
+            this.btnFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnFile.Name = "btnFile";
-            this.btnFile.Size = new System.Drawing.Size(140, 91);
+            this.btnFile.Size = new System.Drawing.Size(187, 114);
             this.btnFile.TabIndex = 0;
             this.btnFile.Text = "选择文件";
             this.btnFile.UseVisualStyleBackColor = true;
@@ -113,14 +118,16 @@
             // 
             // gbLog
             // 
-            this.gbLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.gbLog.Controls.Add(this.btnCancel);
             this.gbLog.Controls.Add(this.btnStart);
             this.gbLog.Controls.Add(this.lblStatus);
             this.gbLog.Controls.Add(this.lstFinished);
-            this.gbLog.Location = new System.Drawing.Point(458, 36);
+            this.gbLog.Dock = System.Windows.Forms.DockStyle.Right;
+            this.gbLog.Location = new System.Drawing.Point(648, 0);
+            this.gbLog.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gbLog.Name = "gbLog";
-            this.gbLog.Size = new System.Drawing.Size(349, 354);
+            this.gbLog.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbLog.Size = new System.Drawing.Size(468, 586);
             this.gbLog.TabIndex = 3;
             this.gbLog.TabStop = false;
             this.gbLog.Text = "处理日志";
@@ -128,9 +135,10 @@
             // btnCancel
             // 
             this.btnCancel.Enabled = false;
-            this.btnCancel.Location = new System.Drawing.Point(246, 310);
+            this.btnCancel.Location = new System.Drawing.Point(328, 388);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(97, 38);
+            this.btnCancel.Size = new System.Drawing.Size(129, 48);
             this.btnCancel.TabIndex = 3;
             this.btnCancel.Text = "取消操作";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -138,9 +146,10 @@
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(6, 310);
+            this.btnStart.Location = new System.Drawing.Point(8, 388);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(97, 38);
+            this.btnStart.Size = new System.Drawing.Size(129, 48);
             this.btnStart.TabIndex = 2;
             this.btnStart.Text = "开始切图";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -149,9 +158,10 @@
             // lblStatus
             // 
             this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(6, 26);
+            this.lblStatus.Location = new System.Drawing.Point(8, 32);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(77, 12);
+            this.lblStatus.Size = new System.Drawing.Size(97, 15);
             this.lblStatus.TabIndex = 1;
             this.lblStatus.Text = "尚未开始抽取";
             // 
@@ -162,10 +172,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lstFinished.GridLines = true;
             this.lstFinished.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem4});
-            this.lstFinished.Location = new System.Drawing.Point(6, 52);
+            listViewItem2});
+            this.lstFinished.Location = new System.Drawing.Point(8, 65);
+            this.lstFinished.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lstFinished.Name = "lstFinished";
-            this.lstFinished.Size = new System.Drawing.Size(337, 252);
+            this.lstFinished.Size = new System.Drawing.Size(451, 314);
             this.lstFinished.TabIndex = 0;
             this.lstFinished.UseCompatibleStateImageBehavior = false;
             this.lstFinished.View = System.Windows.Forms.View.List;
@@ -174,9 +185,11 @@
             // 
             this.gbSave.Controls.Add(this.gbMethod);
             this.gbSave.Controls.Add(this.btnSave);
-            this.gbSave.Location = new System.Drawing.Point(205, 31);
+            this.gbSave.Location = new System.Drawing.Point(273, 39);
+            this.gbSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gbSave.Name = "gbSave";
-            this.gbSave.Size = new System.Drawing.Size(247, 359);
+            this.gbSave.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbSave.Size = new System.Drawing.Size(329, 449);
             this.gbSave.TabIndex = 2;
             this.gbSave.TabStop = false;
             this.gbSave.Text = "选择保存方式";
@@ -185,9 +198,11 @@
             // 
             this.gbMethod.Controls.Add(this.rdbAll);
             this.gbMethod.Controls.Add(this.rdbEach);
-            this.gbMethod.Location = new System.Drawing.Point(12, 208);
+            this.gbMethod.Location = new System.Drawing.Point(16, 260);
+            this.gbMethod.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gbMethod.Name = "gbMethod";
-            this.gbMethod.Size = new System.Drawing.Size(229, 135);
+            this.gbMethod.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbMethod.Size = new System.Drawing.Size(305, 169);
             this.gbMethod.TabIndex = 3;
             this.gbMethod.TabStop = false;
             this.gbMethod.Text = "选择如何保存图片";
@@ -197,9 +212,10 @@
             this.rdbAll.AutoSize = true;
             this.rdbAll.Checked = true;
             this.rdbAll.Cursor = System.Windows.Forms.Cursors.Default;
-            this.rdbAll.Location = new System.Drawing.Point(21, 20);
+            this.rdbAll.Location = new System.Drawing.Point(28, 25);
+            this.rdbAll.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdbAll.Name = "rdbAll";
-            this.rdbAll.Size = new System.Drawing.Size(155, 16);
+            this.rdbAll.Size = new System.Drawing.Size(193, 19);
             this.rdbAll.TabIndex = 1;
             this.rdbAll.TabStop = true;
             this.rdbAll.Text = "全部保存在一个文件夹里";
@@ -209,9 +225,10 @@
             // rdbEach
             // 
             this.rdbEach.AutoSize = true;
-            this.rdbEach.Location = new System.Drawing.Point(21, 42);
+            this.rdbEach.Location = new System.Drawing.Point(28, 52);
+            this.rdbEach.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdbEach.Name = "rdbEach";
-            this.rdbEach.Size = new System.Drawing.Size(191, 16);
+            this.rdbEach.Size = new System.Drawing.Size(238, 19);
             this.rdbEach.TabIndex = 2;
             this.rdbEach.Text = "为每个视频文件单独建立文件夹";
             this.rdbEach.UseVisualStyleBackColor = true;
@@ -219,9 +236,10 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(65, 57);
+            this.btnSave.Location = new System.Drawing.Point(87, 71);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(111, 55);
+            this.btnSave.Size = new System.Drawing.Size(148, 69);
             this.btnSave.TabIndex = 0;
             this.btnSave.Text = "选择保存路径";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -233,9 +251,11 @@
             this.gbFreq.Controls.Add(this.rdbThreeSec);
             this.gbFreq.Controls.Add(this.rdbOneSec);
             this.gbFreq.Controls.Add(this.rdbFiveSec);
-            this.gbFreq.Location = new System.Drawing.Point(12, 31);
+            this.gbFreq.Location = new System.Drawing.Point(16, 39);
+            this.gbFreq.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gbFreq.Name = "gbFreq";
-            this.gbFreq.Size = new System.Drawing.Size(187, 359);
+            this.gbFreq.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbFreq.Size = new System.Drawing.Size(249, 449);
             this.gbFreq.TabIndex = 1;
             this.gbFreq.TabStop = false;
             this.gbFreq.Text = "选择切图频率";
@@ -243,9 +263,10 @@
             // rdbdotfive
             // 
             this.rdbdotfive.AutoSize = true;
-            this.rdbdotfive.Location = new System.Drawing.Point(40, 46);
+            this.rdbdotfive.Location = new System.Drawing.Point(53, 58);
+            this.rdbdotfive.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdbdotfive.Name = "rdbdotfive";
-            this.rdbdotfive.Size = new System.Drawing.Size(113, 16);
+            this.rdbdotfive.Size = new System.Drawing.Size(142, 19);
             this.rdbdotfive.TabIndex = 3;
             this.rdbdotfive.Text = "每0.5秒切一张图";
             this.rdbdotfive.UseVisualStyleBackColor = true;
@@ -254,9 +275,10 @@
             // rdbThreeSec
             // 
             this.rdbThreeSec.AutoSize = true;
-            this.rdbThreeSec.Location = new System.Drawing.Point(40, 159);
+            this.rdbThreeSec.Location = new System.Drawing.Point(53, 199);
+            this.rdbThreeSec.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdbThreeSec.Name = "rdbThreeSec";
-            this.rdbThreeSec.Size = new System.Drawing.Size(101, 16);
+            this.rdbThreeSec.Size = new System.Drawing.Size(126, 19);
             this.rdbThreeSec.TabIndex = 2;
             this.rdbThreeSec.Text = "每3秒切一张图";
             this.rdbThreeSec.UseVisualStyleBackColor = true;
@@ -266,9 +288,10 @@
             // 
             this.rdbOneSec.AutoSize = true;
             this.rdbOneSec.Checked = true;
-            this.rdbOneSec.Location = new System.Drawing.Point(40, 105);
+            this.rdbOneSec.Location = new System.Drawing.Point(53, 131);
+            this.rdbOneSec.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdbOneSec.Name = "rdbOneSec";
-            this.rdbOneSec.Size = new System.Drawing.Size(101, 16);
+            this.rdbOneSec.Size = new System.Drawing.Size(126, 19);
             this.rdbOneSec.TabIndex = 0;
             this.rdbOneSec.TabStop = true;
             this.rdbOneSec.Text = "每1秒切一张图";
@@ -278,9 +301,10 @@
             // rdbFiveSec
             // 
             this.rdbFiveSec.AutoSize = true;
-            this.rdbFiveSec.Location = new System.Drawing.Point(40, 228);
+            this.rdbFiveSec.Location = new System.Drawing.Point(53, 285);
+            this.rdbFiveSec.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdbFiveSec.Name = "rdbFiveSec";
-            this.rdbFiveSec.Size = new System.Drawing.Size(101, 16);
+            this.rdbFiveSec.Size = new System.Drawing.Size(126, 19);
             this.rdbFiveSec.TabIndex = 1;
             this.rdbFiveSec.Text = "每5秒切一张图";
             this.rdbFiveSec.UseVisualStyleBackColor = true;
@@ -303,12 +327,13 @@
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(819, 552);
+            this.ClientSize = new System.Drawing.Size(1116, 803);
             this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmMain";
-            this.Text = "图片抽取工具 by GIAI";
+            this.Text = "Tegu 图片抽取工具 ";
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
